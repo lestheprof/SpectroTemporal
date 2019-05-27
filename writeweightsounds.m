17a437e86955f87b6edc54819c644d8434fa1a66
Leslie S. Smith <l.s.smith@cs.stir.ac.uk>
2019-05-27 11:24:51 +0100

    writing of weight sounds added

--- writeweightsounds.m
+++ writeweightsounds.m
@@ -1,12 +1,40 @@
-function  writeweightsounds(filebasename, params, wtmatrix , numtowrite, basetype)
+function  writeweightsounds(filebasename, params, wtmatrix , numtowrite, varargin)
 %writeweightsounds write a nuymber of wav filkes from the weight matrix
-%   Detailed explanation goes here
+%   generate sounds from 1D arrays contained in the 2Darray wtmatrix. 
+%  basetype 1 uses sin waves at fc from gammatone filter
+%  basetype 2 uses bandpassed limited noise
+% risetime and falltime are linear for now.
+%
+% LSS May 22 2019
+%
 risetime = 0.005 ;
 falltime = 0.005 ;
 fs = 44100 ;
-n_erbs = 0.25 ;
+n_erbs = 0.25 ; % smaller is smaller band size for noise
+basetype = 2 ; % bandlimited noise is default
+% varargin parameter setting
+i = 1 ;
+while(i<=size(varargin,2))
+    switch lower(varargin{i})
+        case 'risetime'
+           risetime = varargin{i+1}; % output sound rise time
+            i=i+1 ; 
+        case 'falltime'
+            falltime = varargin{i+1}; % output sound fall time
+            i=i+1 ; 
+        case 'n_erbs'
+            n_erbs = varargin{i+1}; % noise band size adjustment
+            i=i+1 ; 
+        case 'basetype'
+            basetype = varargin{i+1}; % base type for resynthesis
+            i=i+1 ;  
+        otherwise
+            error('writeweightsounds: Unknown argument %s given',varargin{i});
+    end
+    i=i+1 ;
+end
 for sno = 1: numtowrite
-    filedata = resynthesize(params, squeeze(wtmatrix(sno,:,:)),'basetype', 2, 'n_erbs', n_erbs) ;
+    filedata = resynthesize(params, squeeze(wtmatrix(sno,:,:)),'basetype', basetype, 'n_erbs', n_erbs) ;
     % normalise filedata to max value of 0.9
     filedata = (0.9/max(abs(filedata))) * filedata ;
     % modulate envelope
